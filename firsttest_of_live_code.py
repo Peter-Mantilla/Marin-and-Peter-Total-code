99e389a617863eb27c34634b9c4d3a8e7442d1b4
Peter-Mantilla <mantillapeter1@gmail.com>
2025-07-09 12:58:22 -0400

    Add files via upload

---
+++ firsttest_of_live_code.py
@@ -0,0 +1,20 @@
+print("test")
+print(" WORK")
+
+print("Dumbass code")
+print("test2")
+
+
+
+print("good morning") 
+print("please work")
+
+print("He figured it ou")
+
+
+
+print("hdfxjcykgulih;ogifkuxjhuyilguy")
+
+
+
+print("please")
